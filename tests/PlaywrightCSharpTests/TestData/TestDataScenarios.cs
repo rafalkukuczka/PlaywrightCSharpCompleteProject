1638ef660c58f77f95c5f4dfa363a7c8dc84962b
rkukuczka <rafal.qqczka@gmail.com>
2026-04-29 22:03:08 +0200

    Added TestDataScenarios

---
+++ tests/PlaywrightCSharpTests/TestData/TestDataScenarios.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using SampleMvcApp.Data;
+
+namespace PlaywrightCSharpTests.TestData
+{
+    public static class TestDataScenarios
+    {
+        public static async Task EmptyDatabaseAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await ResetAsync(scope.ServiceProvider);
+        }
+
+        public static async Task DefaultSeedAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+
+            await ResetAsync(scope.ServiceProvider);
+            await DatabaseSeeder.SeedAsync(scope.ServiceProvider);
+        }
+
+        public static async Task RolesOnlyAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+
+            await ResetAsync(scope.ServiceProvider);
+
+            //Seed Roles only
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+        }
+
+        public static async Task AdminUserAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+
+            await ResetAsync(scope.ServiceProvider);
+            await DatabaseSeeder.SeedAsync(scope.ServiceProvider);
+        }
+
+        private static async Task ResetAsync(IServiceProvider services)
+        {
+            var db = services.GetRequiredService<ApplicationDbContext>();
+
+            var users = db.Users.ToList();
+            db.Users.RemoveRange(users);
+
+            var roles = db.Roles.ToList();
+            db.Roles.RemoveRange(roles);
+
+            await db.SaveChangesAsync();
+        }
+
+    }
+}
--- tests/PlaywrightCSharpTests/Tests/Smoke/LoginSmokeTests.cs
+++ tests/PlaywrightCSharpTests/Tests/Smoke/LoginSmokeTests.cs
@@ -1,17 +1,76 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 using PlaywrightCSharpTests.Infrastructure;
 using PlaywrightCSharpTests.Pages;
+using PlaywrightCSharpTests.TestData;
+using SampleMvcApp.Data;
+using SampleMvcApp.Models;
+using System;
 
 namespace PlaywrightCSharpTests.Tests.Smoke;
 
 public class LoginSmokeTests : PageTest
 {
+    private ServiceProvider _services = null!;
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+
+        string s  = Directory.GetCurrentDirectory();
+
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite("Data Source=..\\..\\..\\..\\..\\src\\SampleMvcApp\\samplemvc.db"));
+
+        serviceCollection
+            .AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        _services = serviceCollection.BuildServiceProvider();
+
+        using var scope = _services.CreateScope();
+
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    }
+
     [Test]
     [Category("Smoke")]
     public async Task Admin_Should_Login_With_Seeded_Account()
     {
+        await TestDataScenarios.DefaultSeedAsync(_services);
+
+        var loginPage = new LoginPage(Page);
+
+        await loginPage.LoginAsync(
+            TestConfiguration.BaseUrl,
+            TestConfiguration.AdminEmail,
+            TestConfiguration.AdminPassword);
+
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Orders" })).ToBeVisibleAsync();
+    }
+
+    [Test]
+    [Category("Smoke")]
+    public async Task None_Should_Login_With_EmptySeeded_Accounts()
+    {
+        await TestDataScenarios.EmptyDatabaseAsync(_services);
+
         var loginPage = new LoginPage(Page);
 
         await loginPage.LoginAsync(
